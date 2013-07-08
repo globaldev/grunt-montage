8ce650e9d60caa814f6f55e5f621330397d10bb8
James Allardice <jim.allardice@gmail.com>
2013-07-08 10:48:54 +0100

    Create the sprite

--- tasks/montage.js
+++ tasks/montage.js
@@ -2,16 +2,36 @@ module.exports = function (grunt) {
 
     "use strict";
 
+    var exec = require("child_process").exec,
+        path = require("path"),
+        mkdirp = require("mkdirp");
+
     grunt.registerMultiTask("montage", "Generate CSS sprite sheets and the corresponding stylesheet", function () {
+
+        // It's an async task so make sure Grunt knows this
+        var done = this.async();
 
         // Iterate over all specified file groups.
         this.files.forEach(function (files) {
+
+            // Remove non-existent files from the list
             var src = files.src.filter(function (file) {
-                if (!grunt.file.exists(file)) {
-                    grunt.log.warn("Source file '" + file + "' not found.");
-                    return false;
-                }
-                return true;
+                    if (!grunt.file.exists(file)) {
+                        grunt.log.warn("Source file '" + file + "' not found.");
+                        return false;
+                    }
+                    return true;
+                }),
+                dest = path.join(files.dest, "montage.png");
+
+            // Create the output directory if necessary (ImageMagick errors if it doesn't exist)
+            if (!grunt.file.exists(files.dest)) {
+                mkdirp(files.dest);
+            }
+
+            // Execute the ImageMagick montage tool
+            exec("montage " + src.join(" ") + " " + dest, function (err) {
+                done();
             });
         });
     });
