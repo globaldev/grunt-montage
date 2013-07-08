dd6b0cd29a802c66249a312b72670ea65d4ce3e7
James Allardice <jim.allardice@gmail.com>
2013-07-08 13:24:27 +0100

    Another very basic test

--- test/montage_test.js
+++ test/montage_test.js
@@ -6,8 +6,9 @@ exports.montage = (function () {
 
     return {
         basic: function (test) {
-            test.expect(1);
+            test.expect(2);
             test.equal(grunt.file.exists("tmp/basic/montage.png"), true, "should generate a default montage.png file.");
+            test.equal(grunt.file.exists("tmp/basic/montage.css"), true, "should generage a default montage.css file.");
             test.done();
         }
     };
